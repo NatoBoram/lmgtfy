6a54cd476339219fc46fa5702f0dbeded01e215b
Nato Boram <nato.boram@gmail.com>
2023-03-21 20:51:47 -0400

    👷 Add workflows

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,27 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapterAuto from '@sveltejs/adapter-auto'
+import adapterStatic from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+import { loadEnv } from 'vite'
+
+/** @type {{BUILD_BASE?: '' | `/${string}`}} */
+const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), 'BUILD')
+
+/** @returns {import('@sveltejs/kit').Adapter} */
+function adapter() {
+	if (process.env.GITHUB_ACTIONS) return adapterStatic({ fallback: '404.html' })
+	return adapterAuto()
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	compilerOptions: {},
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		paths: {
+			base: env.BUILD_BASE,
+		},
 	},
 }
 
