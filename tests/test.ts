008765a1fbf66be84eb1e632597ecc8445d9cbe5
Nato Boram <nato.boram@gmail.com>
2023-03-21 22:42:16 -0400

    ✅ Update Playwright config

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,9 @@
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected elements', async ({ page }) => {
 	await page.goto('/')
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
+	await expect(page.getByText('Lmgtfy', { exact: true })).toBeVisible()
+	await expect(page.getByText('Let me Google that for you', { exact: true })).toBeVisible()
+	await expect(page.getByText('Google Search', { exact: true })).toBeVisible()
+	await expect(page.getByText("I'm Feeling Lucky", { exact: true })).toBeVisible()
 })
