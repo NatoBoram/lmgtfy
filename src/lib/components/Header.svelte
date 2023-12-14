0b89b145c9701b7fbb5d820a24235b625bd068b5
Nato Boram <NatoBoram@users.noreply.github.com>
2023-12-13 23:57:15 -0500

    👽️ eslint-plugin-svelte (#14)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,16 +10,21 @@
 	function toggleBrightness(current: Brightness) {
 		switch (current) {
 			case Brightness.dark:
-				return brightness.set(Brightness.light)
+				brightness.set(Brightness.light)
+				return
+
 			case Brightness.light:
-				return brightness.set(Brightness.dark)
+				brightness.set(Brightness.dark)
+				return
 		}
 	}
 </script>
 
 <header class="flex flex-row justify-end gap-4 p-2">
 	<button
-		on:click={() => toggleBrightness($brightness)}
+		on:click={() => {
+			toggleBrightness($brightness)
+		}}
 		class="rounded-full p-2 hover:bg-zinc-200 dark:hover:bg-zinc-700"
 	>
 		<svelte:component this={icons[$brightness]} />
